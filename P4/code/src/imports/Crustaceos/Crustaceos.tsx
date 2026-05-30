89a39d6b8d7d5ab451a3d43d173ab020716b2b5f
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:46:12 +0200

    :sparkles:

---
+++ P4/code/src/imports/Crustaceos/Crustaceos.tsx
@@ -0,0 +1,9 @@
+import imgCrustaceos from "./8ff8a1dc40d16bc4f4d81cd8b82c79f26561a754.png";
+
+export default function Crustaceos() {
+  return (
+    <div className="relative size-full" data-name="crustaceos">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgCrustaceos} />
+    </div>
+  );
+}
