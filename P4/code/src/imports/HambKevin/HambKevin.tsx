ab05df2fa069a09e845414f96a875dd2f1b94b8b
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:49:29 +0200

    :sparkles:

---
+++ P4/code/src/imports/HambKevin/HambKevin.tsx
@@ -0,0 +1,11 @@
+import imgHambKevin from "./e1a42c6143f8fa1e09ac6536607fddbf6a0823f0.png";
+
+export default function HambKevin() {
+  return (
+    <div className="relative size-full" data-name="hamb-kevin">
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        <img alt="" className="absolute h-[115.34%] left-0 max-w-none top-[-15.34%] w-[104.81%]" src={imgHambKevin} />
+      </div>
+    </div>
+  );
+}
