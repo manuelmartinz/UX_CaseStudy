aadcb932c273d6056aa99c4fd7d6501c9ffcd681
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:55:02 +0200

    :sparkles:

---
+++ P4/code/src/imports/M30/M30.tsx
@@ -0,0 +1,9 @@
+import imgM30 from "./b15824dd5587901bccd4d7c5be0833c714eb6884.png";
+
+export default function M() {
+  return (
+    <div className="relative size-full" data-name="M-30">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgM30} />
+    </div>
+  );
+}
