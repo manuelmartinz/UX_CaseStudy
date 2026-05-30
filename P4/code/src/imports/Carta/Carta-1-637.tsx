13df69293c5e375ed813172e28241be482af0c31
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:40:11 +0200

    :sparkles:

---
+++ P4/code/src/imports/Carta/Carta-1-637.tsx
@@ -0,0 +1,18 @@
+import imgImage21 from "./e0f49610b736fbe1a15d07ac0d345cfb4ce50af4.png";
+
+export default function Carta() {
+  return (
+    <div className="relative size-full" data-name="CARTA">
+      <div className="absolute flex inset-0 items-center justify-center" style={{ containerType: "size" }}>
+        <div className="-rotate-90 flex-none h-[100cqw] w-[100cqh]">
+          <div className="relative size-full" data-name="image 21">
+            <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage21} />
+          </div>
+        </div>
+      </div>
+      <div className="absolute inset-[26.33%_25.29%_26.49%_25.29%]" data-name="CARTA-BTN">
+        <p className="absolute font-['Anton:Regular',sans-serif] inset-0 leading-[normal] not-italic text-[200px] text-center text-white">CARTA</p>
+      </div>
+    </div>
+  );
+}
