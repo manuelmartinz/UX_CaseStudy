13ccf22cb579b2bca3843981577abae19f91c40b
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:57:20 +0200

    :sparkles:

---
+++ P4/code/src/imports/Pigma/Pigma.tsx
@@ -0,0 +1,9 @@
+import imgPigma from "./1dc284f73e69d8d8e781f2a8857903882eb5ffa1.png";
+
+export default function Pigma() {
+  return (
+    <div className="relative size-full" data-name="PIGMA">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgPigma} />
+    </div>
+  );
+}
