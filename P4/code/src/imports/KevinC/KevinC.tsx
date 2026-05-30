b07cd84f2b3ae6a6220555506f5a89ee6f5961d9
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:52:58 +0200

    :sparkles:

---
+++ P4/code/src/imports/KevinC/KevinC.tsx
@@ -0,0 +1,9 @@
+import imgKevinC from "./a0032d41b7a7dc50a68df748ef2e2f86ba06450e.png";
+
+export default function KevinC() {
+  return (
+    <div className="relative size-full" data-name="KEVIN.C">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgKevinC} />
+    </div>
+  );
+}
