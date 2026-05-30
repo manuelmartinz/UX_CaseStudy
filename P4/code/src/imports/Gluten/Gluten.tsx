1c35c22fdceb0c1424a7f72e52354b24c22bdfe0
Lola Lorite Díaz <e.lolaloritediaz@go.ugr.es>
2026-05-31 01:48:10 +0200

    :sparkles:

---
+++ P4/code/src/imports/Gluten/Gluten.tsx
@@ -0,0 +1,9 @@
+import imgGluten from "./3fbe42176af1af6857925d7d97a72fdbcdbbb002.png";
+
+export default function Gluten() {
+  return (
+    <div className="relative size-full" data-name="gluten">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgGluten} />
+    </div>
+  );
+}
